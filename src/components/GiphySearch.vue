87bb425d43b0062e2ba090d763cd7a2652ecddd2
Abel Quintero <abel.j.quintero@gmail.com>
2019-04-28 15:38:56 -0700

    creating gifs component and updating the search component

---
+++ src/components/GiphySearch.vue
@@ -0,0 +1,32 @@
+<template>
+    <ul>
+        <Gif v-for="gif in gifs"
+        v-bind:key="gifs.gif"
+        v-bind:gif="gif"/>
+    </ul>
+</template>
+
+<script>
+import Gifs from './Gifs';
+export default {
+    props: {
+        Gifs: Array
+    },
+    
+    components: {
+        Gifs
+    }
+};
+</script>
+
+<style scoped>
+    ul {
+            display: grid;
+            grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
+            grid-column-gap: 15px;
+            grid-row-gap: 15px;
+            list-style-type: none;
+            margin: 0;
+            padding: 0;
+        };
+</style>
