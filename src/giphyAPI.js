f4a9a905972f6c239e5ff20268358d05919fd9ff
Abel Quintero <abel.j.quintero@gmail.com>
2019-04-30 10:34:36 -0700

    forgot to push the eslint file

--- src/giphyAPI.js
+++ src/giphyAPI.js
@@ -1,8 +1,10 @@
+/* eslint-disable no-undef */
 import gifs from './components/Gifs.vue';
 
 
 export default {
     getGifs() {
-        return gifs;
+        return gifs = $.get("http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=RyRi8wCZBqDhCjXXwE5bklzJjfJ1nV0Y&limit=5");
+       
     }
 };
