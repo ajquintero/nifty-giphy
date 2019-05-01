3ca508f31bd56c5922d90d2a4db1085a903679b7
Abel Quintero <abel.j.quintero@gmail.com>
2019-05-01 00:48:54 -0700

    giphy.js updates to connect the API

--- src/giphyAPI.js
+++ src/giphyAPI.js
@@ -1,10 +1,11 @@
 /* eslint-disable no-undef */
+// eslint-disable-next-line no-unused-vars
 import gifs from './components/Gifs.vue';
 
 
 export default {
     getGifs() {
-        return gifs = $.get("http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=RyRi8wCZBqDhCjXXwE5bklzJjfJ1nV0Y&limit=5");
+        return gifs = $.get('http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=RyRi8wCZBqDhCjXXwE5bklzJjfJ1nV0Y&limit=5');
        
     }
 };
